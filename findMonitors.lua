ae8bab7aeca3b0972ac4ed6a89b463427e91d20a
Simon <onomis2@outlook.com>
2024-05-15 20:33:42 +0200

    Made the wizard easier to follow for users

--- findMonitors.lua
+++ findMonitors.lua
@@ -8,4 +8,10 @@ for i, name in pairs(monitorFind) do
         monitors[id] = name
     end
 end
+if id == 0 then
+monitors[1] = false
+end
+if id < 2 then
+    monitors[2] = false
+end
 return monitors
--- main.lua
+++ main.lua
@@ -4,16 +4,24 @@ shell.run("clear")
 print("Starting main...")
 print("Finding Monitors...")
 local monitors = dofile("findMonitors.lua")
-if monitors then
-    print("Found monitors! Please select a monitor to display stored items from following found monitors:")
-    for i,value in pairs(monitors) do
-        print(value)
-    end
-        SelectedMonitor = tostring(io.read())
-    print(SelectedMonitor, "selected to display stored items.")
-else
-    print("Error: No monitors found. Did you attach any to the pc?")
+if monitors[1] == false then
+    print("Could not find monitor. Please attach at least 1 monitor to this pc.")
+    do return end
 end
+
+if monitors[2] == false then
+    print("Found 1 monitor! Using that monitor to display stored items...")
+    SelectedMonitor = monitors[1]
+else
+print("Found monitors! Please select a monitor to display stored items from following found monitors:")
+for i,value in pairs(monitors) do
+    if value ~= false then
+    print(value)
+    end
+    SelectedMonitor = tostring(io.read())
+end
+end
+print(SelectedMonitor, "selected to display stored items.")
 print("Initialising monitor...")
 dofile "storedMonitor.lua"(SelectedMonitor)
 print("Succesfully initialised script! Looping it in 10 seconds!")
