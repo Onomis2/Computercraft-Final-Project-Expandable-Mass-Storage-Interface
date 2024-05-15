768e45244f1451de584a16d78e0bac65541f7abe
Simon <onomis2@outlook.com>
2024-05-15 19:11:49 +0200

    trying something

--- install.lua
+++ install.lua
@@ -2,12 +2,11 @@ print("Type \"run\" to immediatly execute code after installation")
 local run = tostring(io.read())
 if run == "run" then
     shell.run(
-    "wget run https://github.com/Onomis2/Computercraft-Final-Project-Expandable-Mass-Storage-Interface/main/install.lua")
-    print("Succesfully installed. Running code in 5 seconds...")
+    "wget run https://github.com/Onomis2/Computercraft-Final-Project-Expandable-Mass-Storage-Interface/main")print("Succesfully installed. Running code in 5 seconds...")
     sleep(5)
     shell.run("main.lua")
 else
     shell.run(
-    "wget run https://github.com/Onomis2/Computercraft-Final-Project-Expandable-Mass-Storage-Interface/main/install.lua")
+    "wget run https://github.com/Onomis2/Computercraft-Final-Project-Expandable-Mass-Storage-Interface/main")
     print("Succesfully installed. Run main.lua to execute the code.")
 end
