a3caf0402af2aa7d67a223b7b855f2988ceebc5b
Simon <onomis2@outlook.com>
2024-05-16 10:15:48 +0200

    Fixed install file

--- install.lua
+++ install.lua
@@ -1,23 +1,20 @@
-print("Type \"run\" to immediatly execute code after installation")
-local run = tostring(io.read())
-if run == "run" then
+function Download()
     shell.run("wget https://raw.githubusercontent.com/Onomis2/Computercraft-Final-Project-Expandable-Mass-Storage-Interface/main/countItems.lua")
+    shell.run("wget https://raw.githubusercontent.com/Onomis2/Computercraft-Final-Project-Expandable-Mass-Storage-Interface/main/findBarrel.lua")
     shell.run("wget https://raw.githubusercontent.com/Onomis2/Computercraft-Final-Project-Expandable-Mass-Storage-Interface/main/findCentralStorage.lua")
     shell.run("wget https://raw.githubusercontent.com/Onomis2/Computercraft-Final-Project-Expandable-Mass-Storage-Interface/main/findMonitors.lua")
     shell.run("wget https://raw.githubusercontent.com/Onomis2/Computercraft-Final-Project-Expandable-Mass-Storage-Interface/main/install.lua")
     shell.run("wget https://raw.githubusercontent.com/Onomis2/Computercraft-Final-Project-Expandable-Mass-Storage-Interface/main/items.json")
     shell.run("wget https://raw.githubusercontent.com/Onomis2/Computercraft-Final-Project-Expandable-Mass-Storage-Interface/main/main.lua")
     shell.run("wget https://raw.githubusercontent.com/Onomis2/Computercraft-Final-Project-Expandable-Mass-Storage-Interface/main/storedMonitor.lua")
+end
+
+print("Type \"run\" to immediatly execute code after installation")
+local run = tostring(io.read())
+if run == "run" then
     print("Succesfully installed. Running code in 5 seconds...")
     sleep(5)
     shell.run("main.lua")
 else
-    shell.run("wget https://raw.githubusercontent.com/Onomis2/Computercraft-Final-Project-Expandable-Mass-Storage-Interface/main/countItems.lua")
-    shell.run("wget https://raw.githubusercontent.com/Onomis2/Computercraft-Final-Project-Expandable-Mass-Storage-Interface/main/findCentralStorage.lua")
-    shell.run("wget https://raw.githubusercontent.com/Onomis2/Computercraft-Final-Project-Expandable-Mass-Storage-Interface/main/findMonitors.lua")
-    shell.run("wget https://raw.githubusercontent.com/Onomis2/Computercraft-Final-Project-Expandable-Mass-Storage-Interface/main/install.lua")
-    shell.run("wget https://raw.githubusercontent.com/Onomis2/Computercraft-Final-Project-Expandable-Mass-Storage-Interface/main/items.json")
-    shell.run("wget https://raw.githubusercontent.com/Onomis2/Computercraft-Final-Project-Expandable-Mass-Storage-Interface/main/main.lua")
-    shell.run("wget https://raw.githubusercontent.com/Onomis2/Computercraft-Final-Project-Expandable-Mass-Storage-Interface/main/storedMonitor.lua")
     print("Succesfully installed. Run main.lua to execute the code.")
 end
